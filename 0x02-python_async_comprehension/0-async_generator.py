0ba9f89addb1c9dfd5eb9714cc6a28ec0f389ed7
MoanBekele <moanbekele@gmail.com>
2023-08-08 14:57:53 +0300

    :white_check_mark: Fixed PEP

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Task 0's module.
+'''Async Generator with the coroutine will loop 10 times each time asynchronously wait 1 second then yield a random number between 0 and 10.
 '''
 import asyncio
 import random
